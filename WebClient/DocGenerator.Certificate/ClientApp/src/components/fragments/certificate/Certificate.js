194f5315e9dfe3465329c192a994c2f9a47a3413
ThiagoYuri <thiagoyuri0109@gmail.com>
2022-09-01 23:10:15 -0300

    Add: components to listing certificates

---
+++ WebClient/DocGenerator.Certificate/ClientApp/src/components/fragments/certificate/Certificate.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Certificate.css'
+
+export class Courses extends Component {
+
+    constructor(props) {
+        super(props);
+        this.updateStateModal = this.updateStateModal.bind(this)
+    }
+
+    //Href to pdf created
+    updateStateModal() {
+        
+    }
+
+    render() {       
+        const { props } = this
+        return (
+                <div className="" >
+                    
+                </div>
+        );
+    }
+}
